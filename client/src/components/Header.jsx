f3784fcafddb33cc75fe95304b496720d3445d8d
KethminiKW <kethu.kavindya@gmail.com>
2024-01-09 13:11:17 +0530

    create nav bar
    create nav bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,32 @@
 import {FaSearch} from 'react-icons/fa'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header className='bg-slate-300 shadow-md'>
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+            <Link to="/">
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                 <span className='text-slate-500'>Kethu</span>
                 <span className='text-slate-700'>Estate</span>
             </h1>
-            <form className='bg-slate-100 p-3 rounded-lg'>
-                <input type='text' placeholder='Search...' className='bg-transparent' />
+            </Link>
+            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <input type='text' placeholder='Search...' className='bg-transparent  focus:outline-none w-24 sm:w-64' />
                 <FaSearch className='text-slate-600' />
             </form>
+            <ul className='flex gap-4'>
+                <Link to="/">
+                    <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                </Link>
+                <Link to="/about">
+                    <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+                </Link>
+                <Link to="/sign-in">
+                    <li className=' text-slate-700 hover:underline'>Sign In</li>
+                </Link>
+            </ul>
         </div>
     </header>
   )
